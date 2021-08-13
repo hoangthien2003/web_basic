dbf1f051b2eebd1d19df7547d49b98ba04858de4
Thien <hoangthiennt2003@gmail.com>
2021-08-13 18:57:36 +0700

    update new version

--- JS/login.js
+++ JS/login.js
@@ -2,7 +2,7 @@
 
 let username = document.getElementsByClassName("username")
 let password = document.getElementsByClassName("password")
-let btn_login = document.getElementById("button_login")
+const btn_login = document.getElementById("button_login")
 let text_html = document.getElementById("demo")
 const btn_register = document.getElementById("button_register")
 // const btn_confirm
@@ -10,23 +10,11 @@ const btn_register = document.getElementById("button_register")
 btn_login.onclick = function login () {
     if (username[0].value == 'hoangthien2003') {
         if (password[0].value == 'baothien2003') {
-            // btn_confirm = confirm(`Welcome ${username[0].value} to the world ^^`)
-            // if (btn_confirm) {
-            //     text_html.innerHTML = `Hello, ${username[0].value} ^^`
-            //     setTimeout(() => {
-            //         text_html.innerHTML = "Hello World ^^"
-            //     }, 3000)
-            // }
-            confirm(`Welcome ${username[0].value} to the world ^^`)
+            confirm(`Welcome ${username[0].value} to the navbar's world ^^`)
             text_html.innerHTML = `Hello ${username[0].value} ^^`
             setTimeout(() => {
                 location.href = "file:///home/hoangthien/Desktop/fullstack/basic/main.html"
             }, 1000)
-            // else {
-            //     setTimeout(() => {
-            //         text_html.innerHTML = "Hello World ^^"
-            //     }, 3000)
-            // }
         }
         else {
             alert ('Password wrong! Please try again...')
@@ -43,3 +31,28 @@ btn_login.onclick = function login () {
 }
 
 // btn_login.onclick = login
+
+btn_register.onclick = function register () {
+    if (username[0].value == 'hoangthien2003') {
+        if (password[0].value == 'baothien2003') {
+            confirm(`Welcome ${username[0].value} to the button's world ^^`)
+            text_html.innerHTML = `Hello ${username[0].value} ^^`
+            setTimeout(() => {
+                location.href = "file:///home/hoangthien/Desktop/fullstack/basic/button.html"
+            }, 1000)
+        }
+        else {
+            alert ('Password wrong! Please try again...')
+        }
+    }
+    else {
+        alert ('User cannot found !!')
+    }
+    username[0].value = ""
+    password[0].value = ""
+    setTimeout(() => {
+        text_html.innerHTML = 'Hello World ^^'
+    }, 3000)
+}
+
+// btn_register.onclick = register
